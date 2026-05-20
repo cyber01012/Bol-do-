63c5263dc176a57cb6e34bc695aafec6d2577650
Hafsa Ather Khan <hafsaatherkhan@gmail.com>
2026-05-21 01:15:28 +0500

    Merged cyber branch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,24 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:boldo_ai/main.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'package:boldo_ai/main.dart';
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setupFirebaseMocks();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  setUpAll(() async {
+    await Firebase.initializeApp();
   });
+
+  testWidgets('BolDo App smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const BolDoApp());
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
+
+void setupFirebaseMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/firebase_core'),
+    (MethodCall methodCall) async {
+      if (methodCall.method == 'Firebase#initializeCore') {
+        return [
+          {
+            'name': '[DEFAULT]',
+            'options': {
+              'apiKey': 'fake_key',
+              'appId': 'fake_app_id',
+              'messagingSenderId': 'fake_sender_id',
+              'projectId': 'fake_project_id',
+            },
+            'pluginConstants': {},
+          }
+        ];
+      }
+
+      if (methodCall.method == 'Firebase#initializeApp') {
+        return {
+          'name': '[DEFAULT]',
+          'options': {
+            'apiKey': 'fake_key',
+            'appId': 'fake_app_id',
+            'messagingSenderId': 'fake_sender_id',
+            'projectId': 'fake_project_id',
+          },
+            'pluginConstants': {},
+        };
+      }
+
+      return null;
+    },
+  );
+
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/cloud_firestore'),
+    (MethodCall methodCall) async {
+      return null;
+    },
+  );
+
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/firebase_auth'),
+    (MethodCall methodCall) async {
+      return {
+        'user': {
+          'uid': 'fake_uid',
+          'isAnonymous': true,
+        }
+      };
+    },
+  );
 }
